bcd44ab33c409ba115a1be5ef2ce89d7fcc75275
Jeff Lett <jeff@rayatheapp.com>
2023-09-02 17:59:33 -0400

    SPM support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "Shimmer",
+    products: [
+        .library(
+            name: "Shimmer",
+            targets: ["Shimmer"]),
+    ],
+    targets: [
+        .target(
+            name: "Shimmer"),
+    ]
+)
